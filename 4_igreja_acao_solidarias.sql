cc2a46c79dd62c8f1799b53de4701ad7b564c2b1
Mario Jorge Lourinho Catarino Chimene Junior <36141@esjaloures.org>
2026-04-15 17:02:20 +0100

    inserção de dados a base de dados

--- 4_igreja_acao_solidarias.sql
+++ 4_igreja_acao_solidarias.sql
@@ -9,3 +9,12 @@ CREATE TABLE acao_solidarias (
     nomeAcao VARCHAR(100) NOT NULL,
     FOREIGN KEY (id_user) REFERENCES users(id)
 );
+
+INSERT INTO acao_solidarias(id_user,dataHoraInicio,nomeAcao)
+Values
+(1,2024-06-01 10:00:00,Doação de Alimentos),
+(2,2024-07-01 18:00:00,Apoio dos idosos),
+(3,2024-08-01 20:00:00,recolha de alimento),
+(4,2024-08-01 15:00:00,Entrega de Refeições),
+
+
