e26e0e3c9b00efb36b615f15322ddd2666ce9c7d
Gerry <madeiramateus@gmai.com>
2026-04-15 17:48:24 +0100

    inserçao de dados nas tabelas users e apoio sociais

--- 1_igreja_users.sql
+++ 1_igreja_users.sql
@@ -11,3 +11,15 @@ CREATE TABLE users(
     password VARCHAR(255) NOT NULL
 );
 
+-- Inserção de dados na tabela users    
+INSERT INTO users (nome, telefone, email, password) VALUES
+('Arthur Amaral', '912345678', 'arthur.amaral@email.com', '1234'),
+('Fabio Vitoriano', '934567890', 'fabio.vitoriano@email.com', '1234'),
+('Davi Clemente', '987654321', 'davi.clemente@email.com', '1234'),
+('Francisco Prino', '923456789', 'francisco.prino@email.com', '1234'),
+('Gerry Trindade', '915678234', 'gerry.trindade@email.com', '1234'),
+('Henrique Morais', '936789451', 'henrique.morais@email.com', '1234'),
+('Karamjit Singh', '918234567', 'karamjit.singh@email.com', '1234'),
+('Mario Junior', '932145678', 'mario.junior@email.com', '1234'),
+('Marco Figueiredo', '914567890', 'marco.figueiredo@email.com', '1234'),
+('Rodrigo Andraz', '937654123', 'rodrigo.andraz@email.com', '1234');
