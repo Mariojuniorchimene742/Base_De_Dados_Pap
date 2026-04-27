7f1f0aaad885dc0efab63d0cd0bf7f76cbe8d002
Gerry <madeiramateus@gmai.com>
2026-04-27 17:25:27 +0100

    fiz a 3 querie do inner join

--- 8_queries_inners_join.sql
+++ 8_queries_inners_join.sql
@@ -1,11 +1,20 @@
 USE igreja_system;
 
---Q1)useres + acao_solidarias
+--Q1) users + acao_solidarias
 SELECT*
 FROM users u
 INNER JOIN acao_solidarias e ON e.id_user = u.id;
----Q2)acao_solidarias + detalhe_acao_solidarias
+
+---Q2) acao_solidarias + detalhe_acao_solidarias
 SELECT*
 FROM acao_solidarias a
 INNER JOIN detalhe_acao_solidarias d ON d.id_acao_solidaria = a.id;
 
+-- Q3) users + detalhe_acao_solidarias + acao_solidarias
+--mostrar os users que participaram em cada ação solidaria e os detalhes da participaçao
+SELECT *
+FROM users u
+INNER JOIN detalhe_acao_solidarias d 
+    ON d.id_user = u.id
+INNER JOIN acao_solidarias a 
+    ON a.id = d.id_acao_solidaria;
