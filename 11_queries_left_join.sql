cb0b9b2aa560c22a1f5948363bb394db05004074
GerryMateusMTrindade <madeiramateus520@gmail.com>
2026-05-04 14:30:20 +0100

    left join

--- 11_queries_left_join.sql
+++ 11_queries_left_join.sql
@@ -5,5 +5,19 @@ SELECT*
 FROM users u    
 LEFT JOIN acao_solidarias e ON e.id_user = u.id
 
+-- Q5) Users + apoio_sociais
+ --Todos os users com os seus pedidos de apoio social
+SELECT 
+    u.id,
+    u.nome,
+    u.email,
+    u.telefone,
+    a.local,
+    a.codigoPostal,
+    a.membrosDeFamilia,
+    a.pedidoAjuda
+FROM users u
+LEFT JOIN apoio_sociais a ON a.id_user = u.id;
+
 
 
