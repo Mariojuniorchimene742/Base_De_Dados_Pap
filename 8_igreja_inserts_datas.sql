f1dcb2e85e1eb21718035dd108a610dd18b4a804
Mateus <madeiramateus520@gmail.com>
2026-06-02 22:58:30 +0100

    alteraçao na inserçao de dados

--- 8_igreja_inserts_datas.sql
+++ 8_igreja_inserts_datas.sql
@@ -28,7 +28,7 @@ VALUES
 (10, 'Mafra', '2640-666', '911234567', 5, 'Família precisa de apoio geral');
 
 --- Inserção de dados na tabela eventos
-INSERT INTO eventos (id_users,dataHoraInicio,nomeEvento,tipoEvento)
+INSERT INTO eventos (id_users,data_hora_inicio,nome_evento,tipo_evento)
 VALUES
 (1,'2024-07-01 10:00:00','Culto de Domingo','Religioso'),
 (2,'2024-07-02 19:00:00','Reuniao de jovens','Social'),
@@ -55,20 +55,6 @@ Values
 (9,'2024-07-09 19:00:00','Doação de roupas'),
 (10,'2024-07-10 18:00:00','Ação de apoio a refugiados');
 
---- Inserção de dados na tabela detalhe acao solidarias
-INSERT INTO detalhe_acao_solidarias(id_acao_solidaria, descricao, comoAjudar)
-Values
-(1,'Ação de doação de alimentos para famílias carentes.','Doe alimentos não perecíveis, como arroz, feijão, macarrão, enlatados e óleo.'),
-(2,'Visita a asilo para levar alegria e companhia aos idosos.','Participe das visitas levando palavras de conforto, música ou atividades recreativas.'),
-(3,'Campanha de arrecadação de agasalhos para pessoas em situação de rua.','Doe agasalhos em bom estado, como casacos, blusas, cobertores e calçados.'),
-(4,'Ação de limpeza de praia para preservar o meio ambiente.','Participe da limpeza levando sacos de lixo, luvas e disposição para ajudar a manter as praias limpas.'),
-(5,'Doação de sangue para salvar vidas.','Doe sangue em um banco de sangue próximo, seguindo os requisitos de doação.'),
-(6,'Ação de plantio de árvores para contribuir com a sustentabilidade.','Participe do plantio levando mudas de árvores e ajudando a cuidar delas.'),
-(7,'Campanha de arrecadação de brinquedos para crianças carentes.','Doe brinquedos novos ou usados em bom estado para alegrar o Natal de crianças necessitadas.'),
-(8,'Ação de apoio a moradores de rua com distribuição de kits de higiene e alimentos.','Participe da distribuição levando kits contendo itens como sabonete, pasta de dente, escova, roupas e alimentos prontos para consumo.'),
-(9,'Doação de roupas para pessoas em situação de vulnerabilidade social.','Doe roupas em bom estado, especialmente agasalhos, calçados e roupas íntimas.'),
-(10,'Ação de apoio a refugiados com doação de itens essenciais e integração social.','Participe da ação doando itens como alimentos, roupas, produtos de higiene e oferecendo apoio na integração social dos refugiados.');
-
 --- Insersão de dados na tabela pedido orações
 INSERT INTO pedido_oracoes (id_user, email, tipoPedido, descricao)
 VALUES 
@@ -86,7 +72,6 @@ SELECT * FROM users;
 SELECT * FROM apoio_sociais;
 SELECT * FROM eventos;
 SELECT * FROM acao_solidarias;
-SELECT * FROM detalhe_acao_solidarias;
 
 SELECT * FROM pedido_oracoes;
 SELECT * FROM email_verifications;
